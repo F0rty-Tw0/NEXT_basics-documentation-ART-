ce10b72b837fd9ef5618f55d1fce975f40860de3
Artiom Tofan <artiomtofancv@gmail.com>
2020-06-17 21:59:08 +0200

    Added all possible component functions that can render a page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-const Index = () => {
-	return <h1> This is an Index page</h1>;
-};
+import React from 'react';
+
+//==================================================
+//Dumb Components
+//Get Data
+//Return Data
+//==================================================
+// const Index = function() {
+// 	return <h1> This is an Index page</h1>;
+// };
+//--------------------------------------------------
+// const Index = () => {
+// 	return <h1> This is an Index page</h1>;
+// };
+//--------------------------------------------------
+
+//==================================================
+//Class Component
+//More Functionality
+//More Stuff
+//User Lifecycle Functions
+//==================================================
+class Index extends React.Component {
+	render() {
+		return <h1> This is an Index page</h1>;
+	}
+}
 
 export default Index;
