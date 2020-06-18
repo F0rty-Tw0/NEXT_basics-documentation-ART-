169ad5a947026cc51d2f5b6e60f78917787ce053
Artiom Tofan <artiomtofancv@gmail.com>
2020-06-18 10:15:36 +0200

    Showing data fetched from an API inside of getInitialProps using axios

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import SuperComponent from '../components/SuperComponent';
+import axios from 'axios';
 // import Header from '../components/shared/Header';
 
 //==================================================
@@ -45,11 +46,24 @@ import SuperComponent from '../components/SuperComponent';
 // class Index extends React.Component {
 //Calls everything what is included in SuperComponent
 class Index extends SuperComponent {
-	//getInitialProps a static function(that can be called without initiazation) to render the content on server and client side
-	static getInitialProps() {
+	//getInitialProps a static async function(that can be called without initiazation) to render the content on server and client side
+	static async getInitialProps() {
+		//Defining the userData as initial value with let so it can be modified later
+		let userData = {};
+		//Getting data from an URL using axios then displaying it in a console, and catching an error if there is one and displaying it in console too.
+		try {
+			const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+			userData = response.data;
+			console.log('Showing awaited Data from axios');
+			console.log(userData);
+		} catch (error) {
+			console.log(error);
+		}
+
 		console.log('getInitialProps');
 		//Should always return an object
-		return { initailData: [ 1, 2, 3, 4, 5 ] };
+		//In ES6 if your Key is the same as your value f.e. userData: userData then we use shorter expression - userData
+		return { initailData: [ 1, 2, 3, 4, 5 ], userData };
 	}
 
 	//Lifecycle function Constructor
@@ -98,8 +112,10 @@ class Index extends SuperComponent {
 		console.log('render');
 		//Destructurizing a title from this state object which equals to - const title = this.state.title;
 		const { title } = this.state;
-		//Getting data from getInitialProps
-		const initailData = this.props.initailData;
+		//Getting data from getInitialProps original way
+		// const initailData = this.props.initailData;
+		//Getting data from getInitialProps destructorizing both of the Keys
+		const { userData, initialData } = this.props;
 		return (
 			//React.Fragment is used instead of <div /> to hide it from source
 			<React.Fragment>
@@ -110,7 +126,10 @@ class Index extends SuperComponent {
 					{/*<h1 className="title">I am Header Subtitle</h1>*/}
 					{/*</Header>*/}
 					<h1 className="title">This is an Index page(Class Component)</h1>
-					<h1 className="title">{title}</h1>
+					{/* Showing the title from the defined state */}
+					<h2 className="title">{title}</h2>
+					{/* Showing the userData title from getInitialProps */}
+					<h3>{userData.title}</h3>
 					{/* Calling updateTitle Function to change the state defined in it, this function will rerender the page with new values */}
 					{/* <button onClick={() => this.updateTitle()}>Change Title</button> */}
 					<button onClick={this.updateTitle}>Change Title</button>
