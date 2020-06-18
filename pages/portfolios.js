8b96882a592c77cb8829fcc8feec4fd832f0f9bd
Artiom Tofan <artiomtofancv@gmail.com>
2020-06-18 10:41:53 +0200

    Displaying data from API, iterating(looping) throught it, and displaying it in our JSX

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
+//Duplicating the axios use from the Index, its better to have it somewhere centralized where we can use it
+import axios from 'axios';
 
 class Portfolios extends React.Component {
+	//getInitialProps a static async function(that can be called without initiazation) to render the content on server and client side
+	static async getInitialProps() {
+		//Defining the posts as initial value with let as array so it can be modified later
+		let posts = [];
+		//Getting data from an URL using axios then displaying it in a console, and catching an error if there is one and displaying it in console too.
+		try {
+			const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+			posts = response.data;
+			console.log('Showing awaited Data from axios');
+			console.log(posts);
+		} catch (error) {
+			console.log(error);
+		}
+
+		console.log('getInitialProps');
+		//Should always return an object
+		//Showing the spliced posts from 0 index until 10 index
+		return { posts: posts.splice(0, 10) };
+	}
+
+	//A function to iterate(loop) between posts to display them all
+	renderPosts(posts) {
+		return posts.map((justOnePost) => {
+			return <li> {justOnePost.title}</li>;
+		});
+	}
+
 	render() {
+		//Consoling the posts data
+		console.log(this.props);
+		//Getting destructurized posts from props
+		const { posts } = this.props;
 		return (
 			<BaseLayout>
 				<h1 className="title"> This is a Portfolios page(Class Component)</h1>
+				{/* Getting the value from posts */}
+				<ul>{this.renderPosts(posts)}</ul>
 			</BaseLayout>
 		);
 	}
